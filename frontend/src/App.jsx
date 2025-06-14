d3ebc78127770734c651d9b8849ea0aa80c655c1
Vedant <bhavsarvedant9@gmail.com>
2025-06-14 22:18:09 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,61 +10,59 @@ import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 import { Toaster } from "react-hot-toast";
 import { toast } from "react-hot-toast";
-import { useQuery } from "@tanstack/react-query";
-import { axiosInstance } from "./lib/axios.js";
+import PageLoader from "./components/PageLoader.jsx";
+import useAuthUser from "./hooks/useAuthUser.js";
 
 const App = () => {
-  const {
-    data: authData,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["authUser"],
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-    retry: false,
-  });
+  
+  const { isLoading, authUser } = useAuthUser()
+  
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded
 
-  const authUser = authData?.user;
+  if (isLoading) {
+    return <PageLoader/>
+  }
+    return (
+      <div className="h-screen" data-theme="corporate">
+        {/* <div className="navbar bg-red-500">hi </div> */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/" element={authUser?<HomePage />:<Navigate to="/login"} /> */}
 
-  return (
-    <div className="h-screen" data-theme="corporate">
-      {/* <div className="navbar bg-red-500">hi </div> */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* <Route path="/" element={authUser?<HomePage />:<Navigate to="/login"} /> */}
+          <Route
+            path="/signup"
+            element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/notifications"
+            element={
+              isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/call"
+            element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/chat"
+            element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/onboarding"
+            element={
+              isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />
+            }
+          />
+        </Routes>
 
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/notifications"
-          element={authUser ? <NotificationsPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/call"
-          element={authUser ? <CallPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/chat"
-          element={authUser ? <ChatPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/onboarding"
-          element={authUser ? <OnboardingPage /> : <Navigate to="/login" />}
-        />
-      </Routes>
-
-      <Toaster />
-    </div>
-  );
+        <Toaster />
+      </div>
+    );
 };
 
 export default App;
--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+
+import React from "react";
+import { HashLoader } from "react-spinners";
 
 const PageLoader = () => {
   return (
-    <div>
-      
+    <div className="flex justify-center items-center h-screen">
+      <HashLoader color="#00bcd4" size={60} />
     </div>
-  )
-}
+  );
+};
 
-export default PageLoader
+export default PageLoader;
+
