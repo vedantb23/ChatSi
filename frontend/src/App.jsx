75bac7a17f29f81065a3044a11a2c20dcbab1374
Vedant <bhavsarvedant9@gmail.com>
2025-06-14 17:41:20 +0530

    ui fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Routes, Route } from "react-router";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
@@ -7,41 +7,62 @@ import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import { toast } from "react-hot-toast";
-import {
-  useQuery
-} from "@tanstack/react-query";
-import {axiosInstance}  from './lib/axios.js'; 
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 
 const App = () => {
-  const { data, isLoading, error } = useQuery({
-    querykey: ["todos"],
+  const {
+    data: authData,
+    isLoading,
+    error,
+  } = useQuery({
+    querykey: ["authUser"],
     queryFn: async () => {
       const res = await axiosInstance.get("/auth/me");
       return res.data;
-      
-    }
-  })
+    },
+    retry: false,
+  });
 
-  console.log(data);
+  const authUser = authData?.user;
+
   return (
     <div className="h-screen" data-theme="fantasy">
-
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
-      </Routes>
+        {/* <Route path="/" element={authUser?<HomePage />:<Navigate to="/login"} /> */}
 
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationsPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/call"
+          element={authUser ? <CallPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/chat"
+          element={authUser ? <ChatPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/onboarding"
+          element={authUser ? <OnboardingPage /> : <Navigate to="/login" />}
+        />
+      </Routes>
 
       <Toaster />
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
-}
+  const [signupData, setsignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
 
-export default SignUpPage
+  return (
+    <div
+      className="h-screen flex justify-center items-center p-4 
+    sm:p-6 md:p-8 "
+      data-theme="night"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* sign up left  */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* logo */}
+          <div className="mb-4 flex items-center justify-start gap-2 ">
+            <img
+              src="/logo-bh-white-unscreen.gif"
+              alt="Logo animation"
+              className="w-full h-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
