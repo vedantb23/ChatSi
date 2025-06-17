24dca651f0a3b5a84ff4cc45e1f35465bc7a209a
Vedant <bhavsarvedant9@gmail.com>
2025-06-17 15:59:39 +0530

    Update auth.controller.js

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -51,11 +51,11 @@ export async function signup(req, res) {
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: "2d" })
         
         res.cookie("jwt", token, {
-            maxAge: 2 * 24 * 60 * 60 * 1000,
-            httpOnly: true,
-            sameSite: "strict",
-            secure:process.env.NODE_ENV==="production"
-        })
+          maxAge: 2 * 24 * 60 * 60 * 1000,
+          httpOnly: true,
+          sameSite: "none",
+          secure: true,
+        });
 
         res.status(201).json({success:true,user:newUser})
     } catch (error) {
@@ -88,8 +88,8 @@ export async  function login(req, res) {
       res.cookie("jwt", token, {
         maxAge: 2 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV === "production",
+        sameSite: "none",
+        secure: true,
       });
       res.status(200).json({ sucess: true, user });
 
