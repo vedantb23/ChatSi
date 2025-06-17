80debda91383201dbded74055a54be378549b551
Vedant <bhavsarvedant9@gmail.com>
2025-06-17 15:50:27 +0530

    bugs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,20 @@
-import expres from "express";
+import express from "express";
+
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
-import userRoutes from "./routes/user.route.js"
-import chatRoutes from "./routes/chat.route.js"
-import cors from "cors"
-import path from "path"
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import cors from "cors";
+import path from "path";
 dotenv.config();
-const app = expres();
+const app = express();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
-app.get("/",(req,res) => {
-  res.send("Hello bhai Backend ")
-  
-}
-)
-
+app.get("/", (req, res) => {
+  res.send("Hello bhai Backend ");
+});
 
 app.use(
   cors({
@@ -25,23 +23,22 @@ app.use(
     credentials: true,
   })
 );
-app.use(expres.json());
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-
 // if (process.env.NODE_ENV === "production") {
 //   app.use(expres.static(path.join(__dirname, "../frontend/dist")));
 //   app.get("*",(req,res) => {
 //     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    
+
 //   }
 //   )
 // }
-  app.listen(PORT, () => {
-    console.log(`Server running at port ->${PORT}`);
-    connectDB();
-  });
+app.listen(PORT, () => {
+  console.log(`Server running at port ->${PORT}`);
+  connectDB();
+});
