40cd354916360a221efb3dd7f4710da49bf401d1
Vedant <bhavsarvedant9@gmail.com>
2025-06-17 14:51:28 +0530

    final deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,16 +6,18 @@ import cookieParser from "cookie-parser"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import cors from "cors"
-
+import path from "path"
 dotenv.config();
 const app = expres();
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 app.get("/",(req,res) => {
   res.send("Hello bhai Backend ")
   
 }
 )
+
+
 app.use(cors({
   //chanegchange
   origin: "http://localhost:5173",
@@ -28,8 +30,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running at port ->${PORT}`);
-  connectDB();
 
-});
+// if (process.env.NODE_ENV === "production") {
+//   app.use(expres.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*",(req,res) => {
+//     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    
+//   }
+//   )
+// }
+  app.listen(PORT, () => {
+    console.log(`Server running at port ->${PORT}`);
+    connectDB();
+  });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://your-backend.up.railway.app/api";
 export const axiosInstance = axios.create({
     baseURL: `${apiUrl}`,
     withCredentials:true,
